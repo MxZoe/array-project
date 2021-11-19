f80efa46fe66cf95cd995645165594d7a6bf4aaa
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-18 20:07:27 -0700

    implemented substitute tests

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function substitute(number){
+  if(number === 1){
+    return "Beep"
+  } 
+  else if(number === 2){
+    return "Boop"
+  }
+  else if(number === 3){
+    return "Won't you be my neighbor?"
+  } 
+  else{
+    return number;
+  }
+}
