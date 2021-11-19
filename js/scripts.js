c2625aba15a5a205b2883c3ec72ce0fdacdf70b9
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-18 20:30:00 -0700

    add hasNumber function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//functional logic
 function substitute(number){
   if(number === 1){
     return "Beep"
@@ -13,12 +14,30 @@ function substitute(number){
   }
 }
 
+function hasNumber(number){
+  let numberString = number.toString();
+  if(numberString.includes("3")){
+    return substitute(3);    
+  }
+  else if(numberString.includes("2")){
+    return substitute(2);
+  }
+  else if(numberString.includes("1")){
+    return substitute(1);
+  }
+  else{
+    return number;
+  }
+}
 
+//business logic
+
+//UI logic
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     let userNumber = parseInt($("#numberInput").val());
-    let substituteAnswer = substitute(userNumber);
-    $("#result").html(substituteAnswer);
+    let answer = hasNumber(userNumber);
+    $("#result").html(answer);
     
     event.preventDefault();
   })
