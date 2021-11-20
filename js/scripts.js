d619dc01ee9e1534f78e0118d7dfdd72fcb5a33c
Zoe Weinstein <mxzweinstein@gmail.com>
2021-11-20 14:48:31 -0700

    added checks for non-valid input

--- js/scripts.js
+++ js/scripts.js
@@ -32,12 +32,12 @@ function hasNumber(number){
 
 //business logic
 function beepBoop(userNumber){
+ // let noPunctuation = userNumber.replace(/[\D]/g, "");
   let numberArray = []
-
+  
   for(let i = 0; i <= userNumber; i++){
     numberArray.push(i);
   }
-  
   substitutedArray = numberArray.map(function(number){
     return " " + hasNumber(number);
   });
@@ -47,13 +47,24 @@ function beepBoop(userNumber){
 //UI logic
 $(document).ready(function(){
   $("#formOne").submit(function(event){
-    let userNumber = $("#numberInput").val();
-    let answer = beepBoop(userNumber);
-    if(userNumber === ""){
-      $("#result").html("Please enter one or more numbers!");
+    let userNumber = $("#numberInput").val().trim();
+    //check if there is whitespace in the string (indicating multiple numbers jhave been entered)
+    if(userNumber.indexOf(" ") > 0){
+      $("#result").html("Please do not enter number than one number.")
+      $("#numberInput").val("");
+      return false;
     }
-    $("#result").html(answer);
+    //check if the submit form was blank  
+    else if(userNumber === ""){
+      $("#result").html("Please enter one or more numbers!");
+      $("#numberInput").val("");
+      return false;
+    } 
+    //remove any non-digits that may have been entered
+    userNumber = userNumber.replace(/[\D]/g, ""); 
+    let answer = beepBoop(userNumber);
     $("#numberInput").val("");
+    $("#result").html("Your number was  " + userNumber + ".<br>" + answer);
     event.preventDefault();
   })
 
